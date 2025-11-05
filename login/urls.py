f38602f484c815481897ba432be05652c23f9913
Arshad khan <arshadkhan200205@gmail.com>
2025-11-05 23:49:41 +0530

    Login Frontend Created

--- login/urls.py
+++ login/urls.py
@@ -2,5 +2,8 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path('', views.render_homepage, name='homepage')
+    path('', views.render_homepage, name='homepage'),
+    path('login/', views.render_login_page, name='login'),
+    path('signup/', views.render_signup_page, name='signup'),
+    path('reset-password/', views.render_reset_password_page, name='reset_password'),
 ]
--- login/views.py
+++ login/views.py
@@ -3,3 +3,12 @@ from django.shortcuts import render
 # Create your views here.
 def render_homepage(request):
     return render(request, 'index.html')
+
+def render_login_page(request):
+    return render(request, 'login.html')
+
+def render_signup_page(request):
+    return render(request, 'signup.html')
+
+def render_reset_password_page(request):
+    return render(request, 'resetPassword.html')
