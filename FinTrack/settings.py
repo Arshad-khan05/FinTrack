e64458c57e42e54e01b7cd154d4e6f17b39738fa
Arshad khan <arshadkhan200205@gmail.com>
2026-02-04 00:23:18 +0530

    Remove WhiteNoise middleware for now

--- FinTrack/settings.py
+++ FinTrack/settings.py
@@ -48,7 +48,6 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    'whitenoise.middleware.WhiteNoiseMiddleware',  # Add WhiteNoise for static files
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
