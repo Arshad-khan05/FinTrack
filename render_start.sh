580667186051258f106cbb76520f30b6a33c5095
Arshad khan <arshadkhan200205@gmail.com>
2026-02-03 23:45:45 +0530

    Added script file

---
+++ render_start.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+set -e
+
+# Run DB migrations and collect static files before starting the app
+python manage.py migrate --noinput
+python manage.py collectstatic --noinput
+
+# Start Gunicorn binding to the Render-provided port
+exec gunicorn FinTrack.wsgi:application --bind 0.0.0.0:$PORT
